7ca93338cf28cde6b65689c5c1f6e3c2f2407d0e
0KSY <uryhon7@gmail.com>
2024-02-03 12:09:24 +0900

    개발 환경 세팅

---
+++ bulletin-board2/src/main/java/com/solo/bulletinboard2/BulletinBoard2Application.java
@@ -0,0 +1,13 @@
+package com.solo.bulletinboard2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoard2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulletinBoard2Application.class, args);
+	}
+
+}
---
+++ bulletin-board2/src/test/java/com/solo/bulletinboard2/BulletinBoard2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solo.bulletinboard2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoard2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
